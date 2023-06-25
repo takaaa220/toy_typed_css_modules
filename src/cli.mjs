0521f4f92ecdee212279ab1a90c188a3970a70fe
takaaa220 <taaaaaaaaaa220@gmail.com>
2023-06-25 19:21:02 +0900

    implement watcher

--- src/cli.mjs
+++ src/cli.mjs
@@ -1,3 +1,32 @@
-import { run } from "./run.mjs";
+/**
+ * @typedef {import("yargs")} default
+ */
 
-await run(process.argv[2]);
+import { run } from "./run.mjs";
+import yargs from "yargs";
+
+async function main() {
+  const argv = yargs(process.argv)
+    .option({
+      pattern: {
+        type: "string",
+        describe: "Glob pattern",
+        demandOption: true,
+        alias: "p",
+      },
+    })
+    .option({
+      watch: {
+        type: "boolean",
+        describe: "Watch for file changes",
+        default: false,
+        alias: "w",
+      },
+    })
+    .help()
+    .parseSync();
+
+  await run(argv.pattern, { watch: argv.watch });
+}
+
+main();
--- src/run.mjs
+++ src/run.mjs
@@ -5,6 +5,7 @@
  * @typedef {import("fs")} writeFileSync
  * @typedef {import("fs")} readFileSync
  * @typedef {import("url")} fileURLToPath
+ * @typedef {import("chokidar")} default
  */
 
 import { readFileSync, writeFileSync } from "fs";
@@ -12,25 +13,74 @@ import { generateTypes } from "./core.mjs";
 import { glob } from "glob";
 import { join, dirname } from "path";
 import { fileURLToPath } from "url";
+import chokidar from "chokidar";
+
+/**
+ * @param {string} pattern
+ * @param {{ watch: boolean; }} _options
+ */
+export async function run(pattern, _options) {
+  if (!pattern) {
+    throw new Error("pattern is required.");
+  }
+
+  const options = {
+    watch: _options?.watch ?? false,
+  };
+
+  if (options.watch) {
+    await watch(pattern);
+    return;
+  } else {
+    await runAll(pattern);
+  }
+}
 
 /**
  * @param {string} pattern
  */
-export async function run(pattern) {
+async function watch(pattern) {
+  if (!pattern) {
+    throw new Error("pattern is required.");
+  }
+
+  const watcher = chokidar.watch(pattern);
+  watcher.on("change", (path) => {
+    runSingleFile(path);
+  });
+
+  watcher.on("add", (path) => {
+    runSingleFile(path);
+  });
+
+  console.info(`Watching: ${pattern}`);
+}
+
+/**
+ * @param {string} pattern
+ */
+async function runAll(pattern) {
   if (!pattern) {
     throw new Error("pattern is required.");
   }
 
   const files = await glob(pattern);
 
-  files.forEach((fileName) => {
-    const modulesCss = readFileSync(fileName, "utf8");
+  files.forEach(runSingleFile);
+}
 
-    const dts = generateTypes(modulesCss);
-    const dtsFilePath = join(fileName + ".d.ts");
+/**
+ * @param {string} fileName
+ */
+function runSingleFile(fileName) {
+  if (!fileName || !fileName.endsWith(".css")) return;
 
-    writeFileSync(dtsFilePath, dts, { encoding: "utf8" });
+  const modulesCss = readFileSync(fileName, "utf8");
 
-    console.log(`Generated: ${dtsFilePath}`);
-  });
+  const dts = generateTypes(modulesCss);
+  const dtsFilePath = join(fileName + ".d.ts");
+
+  writeFileSync(dtsFilePath, dts, { encoding: "utf8" });
+
+  console.info(`Generated: ${dtsFilePath}`);
 }
